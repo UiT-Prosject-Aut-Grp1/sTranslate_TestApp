a1966ecb2a783d1e425e4b3da8201cd665bf5520
akkarvik <akkarvik@hotmail.com>
2016-03-11 16:09:18 +0100

    New log files

--- sTranslate_TestApp/sTranslate_TestApp/Program.fs
+++ sTranslate_TestApp/sTranslate_TestApp/Program.fs
@@ -126,16 +126,30 @@ let main argv =
     let numLoops = 100
     let fileName = testFile    
     
-    //let (searchCounter,elapsedTime,loopTimes) = StressTestCsGetToText fileName numLoops
-    //let (searchCounter,elapsedTime,loopTimes) = StressTestCsToText fileName numLoops
-    //let (searchCounter,elapsedTime,loopTimes) = StressTest sTranslate_direct.XltTool.GetToText sTranslate_direct.XltEnums.ToPropertyType sTranslate_direct.XltEnums.ToCriteria fileName numLoops
-    //let (searchCounter,elapsedTime,loopTimes) = StressTest sTranslate_direct.XltTool.ToText sTranslate_direct.XltEnums.ToPropertyType sTranslate_direct.XltEnums.ToCriteria fileName numLoops
-    //let (searchCounter,elapsedTime,loopTimes) = StressTest sTranslate_fs.XltTool.GetToText sTranslate_fs.XltEnums.ToPropertyType sTranslate_fs.XltEnums.ToCriteria fileName numLoops
+//    let (searchCounter,elapsedTime,loopTimes) = StressTestCsGetToText fileName numLoops
+//    let logFile = logFileCSharpNoCache
+//    let (searchCounter,elapsedTime,loopTimes) = StressTestCsToText fileName numLoops
+//    let logFile = logFileCSharpCache
+//    let (searchCounter,elapsedTime,loopTimes) = StressTest sTranslate_direct.XltTool.GetToText sTranslate_direct.XltEnums.ToPropertyType sTranslate_direct.XltEnums.ToCriteria fileName numLoops
+//    let logFile = logFileFSharpDirectNoCache
+//    let (searchCounter,elapsedTime,loopTimes) = StressTest sTranslate_direct.XltTool.ToText sTranslate_direct.XltEnums.ToPropertyType sTranslate_direct.XltEnums.ToCriteria fileName numLoops
+//    let logFile = logFileFSharpDirectCache
+//    let (searchCounter,elapsedTime,loopTimes) = StressTest sTranslate_fs.XltTool.GetToText sTranslate_fs.XltEnums.ToPropertyType sTranslate_fs.XltEnums.ToCriteria fileName numLoops
+//    let logFile = logFileFSharpNoCache
     let (searchCounter,elapsedTime,loopTimes) = StressTest sTranslate_fs.XltTool.ToText sTranslate_fs.XltEnums.ToPropertyType sTranslate_fs.XltEnums.ToCriteria fileName numLoops
-    
+    let logFile = logFileFSharpCache
+
+//    let fromText = "Edit"
+//    let context = "lblTitle"
+//    let property = sTranslate_fs.XltEnums.ToPropertyType "Id"
+//    let criteria = sTranslate_fs.XltEnums.ToCriteria "Contains"
+//    let toLang = "no"
+//    let toText = sTranslate_fs.XltTool.ToText criteria fromText property context toLang
+//    printfn "%s" toText
+
     printfn "Duration: %A sec" elapsedTime
     
-    let outFile = new StreamWriter(logFileCSharpNoCache)
+    let outFile = new StreamWriter(logFile)
     let dataFrame = loopTimes
                     |> Seq.iter (fun y -> outFile.WriteLine(y.ToString().Split([|':'|]).[2]))
     outFile.Close() |> ignore
