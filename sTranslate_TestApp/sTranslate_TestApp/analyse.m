3eb4a1f179e71ef74317d7d313ba074dc6dfd901
MIkkel Eltervåg <mikkeleltervaag@gmail.com>
2016-05-11 14:44:22 +0200

    updated matlab script

--- sTranslate_TestApp/sTranslate_TestApp/analyse.m
+++ sTranslate_TestApp/sTranslate_TestApp/analyse.m
@@ -7,7 +7,8 @@ logging{2} = csvread('logCSharpWithCache.csv');
 logging{3} = csvread('logFSharpDirectNoCache.csv');
 logging{4} = csvread('logFSharpDirectWithCache.csv');
 logging{5} = csvread('logFSharpNoCache.csv');
-logging{6} = csvread('logFSharpCache.csv');
+logging{6} = csvread('logFSharpWithCache.csv');
+logging{7} = csvread('logFSharpParallel.csv');
 
 % Set names
 name{1} = 'C# no cache';
@@ -15,22 +16,13 @@ name{2} = 'C# width cache';
 name{3} = 'F# Direct no cache';
 name{4} = 'F# Direct width cache';
 name{5} = 'F# No Cache';
-name{6} = 'F# cache';
+name{6} = 'F# with cache';
+name{7} = 'F# Parallel';
 
-% Set names
-color{1} = [1 0 0];
-color{2} = [0 1 0];
-color{3} = [0 0 1];
-color{4} = [1 1 0];
-color{5} = [0 1 1];
-color{6} = [1 0 1];
 
 % Name graphics window
 figure('name','Histogram');
 for logNumber = 1:length(logging)
-    
-    % Convert to ms
-    logging{logNumber} = logging{logNumber}*1000;
     
     % Remove first element
     first(logNumber) = logging{logNumber}(1);
@@ -52,10 +44,7 @@ for logNumber = 1:length(logging)
     end
     
    % Make histogram
-   % set(findobj(gca,'Type','patch'),'FaceColor',color{logNumber},'EdgeColor','w');
-   % hist(logging{logNumber})
-    
-   subplot(3,2,logNumber)
+   subplot(3,3,logNumber)
    hist(logging{logNumber},20)
    title(name{logNumber})
    
@@ -67,23 +56,27 @@ hold off;
 figure('name','Average');
 set(gca,'LooseInset',get(gca,'TightInset'))
 
+%y = [average(1) average(2); average(3) average(4); average(5)  average(6); 0 average(7)];
 y = [average(1) average(2); average(3) average(4); average(5)  average(6)];
 bar(y)
+%set(gca,'XTickLabel',{'C#', 'F# direct', 'F#', 'F# Paralell'})
 set(gca,'XTickLabel',{'C#', 'F# direct', 'F#'})
-legend('No cache', 'Width cache');
+legend('No cache', 'With cache');
 
-set(gca,'YScale','log')
+ylabel('seconds') % label for y axis
+
+%set(gca,'YScale','log')
 
 
-% Average chart
+% Fist chart
 hold off;
 figure('name','First');
 set(gca,'LooseInset',get(gca,'TightInset'))
 
-y = [first(1) first(2); first(3) first(4); first(5)  first(6)];
+y = [first(1) first(2); first(3) first(4); first(5)  first(6); 0 first(7)];
 bar(y)
-set(gca,'XTickLabel',{'C#', 'F# direct', 'F#'})
-legend('No cache', 'Width cache');
+set(gca,'XTickLabel',{'C#', 'F# direct', 'F#', 'F# Paralell'})
+legend('No cache', 'With cache');
 
-set(gca,'YScale','log')
+%set(gca,'YScale','log')
 
